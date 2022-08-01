ace7522d807dc22ac9f8a506b1d3610a81bbd8f2
Sophia G20 Grzeskowiak-Amezquita <SophiaG20@SophiaG20.com>
2022-08-01 03:25:54 -0700

    sumAndMultiply Passes

--- js/app.js
+++ js/app.js
@@ -57,11 +57,20 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let sumABC = sum(a, b)[0];
+  sumABC = sum(sumABC, c)[0];
+  let sumString = a + ' and ' + b + ' and ' + c + ' sum to ' + sumABC + '.';
+  let mulABC = multiply(a, b)[0];
+  mulABC = multiply(mulABC, c)[0];
+  let mulString = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + mulABC + '.';
+  let result = [sumABC, mulABC, sumString, mulString];
+  return result;
+
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
